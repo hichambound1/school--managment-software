875e07ccea08f840ff11431d97efba32c65b3b41
Hicham.BOUNDOUQ <hicham.boundouq@um6p.ma>
2023-03-25 09:34:10 +0000

    role and permissions seeder

---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles=[
+            'super admin',
+            'admin',
+            'client super admin',
+            'client admin',
+            'assistent',
+            'teacher',
+            'parent',
+            'driver',
+            'student',
+        ];
+        $permissions=[
+            //user
+            "can_add_user",
+            "can_edit_user",
+            "can_delete_user",
+            //role
+            "can_delete_role",
+            "can_edit_role",
+            "can_add_role",
+            //permissions
+            "can_add_permission",
+            "can_edit_permission",
+            "can_delete_permission",
+            //city
+            "can_add_city",
+            "can_edit_city",
+            "can_delete_city",
+            //country
+            "can_add_country",
+            "can_edit_country",
+            "can_delete_country",
+            //language
+            "can_add_language",
+            "can_edit_language",
+            "can_delete_language",
+            //plan
+            "can_add_plan",
+            "can_edit_plan",
+            "can_delete_plan",
+            //option
+            "can_add_option",
+            "can_edit_option",
+            "can_delete_option",
+        ];
+
+        foreach ($permissions as $permission) {
+            $old_permission= Permission::where('name',$permission)->first();
+            if(!isset($old_permission)){
+                Permission::create(['name' => $permission]);
+            }
+        }
+        foreach ($roles as $role_name) {
+            $role= Role::where('name',$role_name)->first();
+            if(!isset($role)){
+                $role = Role::create(['name' => $role_name]);
+            }
+            if($role_name==='super admin'){
+                $role->syncPermissions($permissions);
+            }
+        }
+
+    }
+}
