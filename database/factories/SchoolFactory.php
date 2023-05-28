52b4d99f3529c5393d7c6d3b6b1e3cfe3a99fa49
Hicham.BOUNDOUQ <hicham.boundouq@um6p.ma>
2023-05-28 18:06:26 +0100

    schools wip

---
+++ database/factories/SchoolFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\School>
+ */
+class SchoolFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'owner_id' => \App\Models\User::factory(),
+            'city_id' => \App\Models\City::factory(),
+            'logo' => $this->faker->imageUrl(),
+        ];
+    }
+}
---
+++ database/seeders/SchoolSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\School;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SchoolSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create();
+        \App\Models\City::factory(5)->create();
+        School::factory(10)->create();
+    }
+}
